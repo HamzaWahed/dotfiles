7b1fbe66e3078fb795d804f762b14ef797ae770c
hamza wahed <hzd454@gmail.com>
2024-11-17 13:33:15 -0400

    remove super-tab keybindings

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -39,31 +39,20 @@ return {
             }
 
             opts.mapping = vim.tbl_extend("force", opts.mapping, {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-                        cmp.confirm({ select = true })
-                    elseif vim.snippet.active({ direction = 1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(1)
-                        end)
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif vim.snippet.active({ direction = -1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(-1)
-                        end)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                ["<Tab>"] = function(fallback)
+                    return LazyVim.cmp.map(
+                        { "snippet_forward", "ai_accept" },
+                        fallback
+                    )()
+                end,
+
+                ["<S-Tab>"] = function(fallback)
+                    return LazyVim.cmp.map(
+                        { "snippet_backward", "ai_accept" },
+                        fallback
+                    )()
+                end,
+
                 ["<CR>"] = cmp.config.disable,
             })
         end,
