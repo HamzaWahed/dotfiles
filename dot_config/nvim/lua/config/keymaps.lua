4b364ec9f85272a101490a32be5960c1d3eaa941
hamza wahed <hzd454@gmail.com>
2025-01-13 11:47:34 -0400

    Remove kubectl.nvim from config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -36,22 +36,6 @@ vim.api.nvim_set_keymap(
     { noremap = true, silent = true }
 )
 
-vim.api.nvim_set_hl(0, "LeapBackdrop", { fg = "None" })
-
-vim.api.nvim_set_hl(0, "LeapMatch", {
-    fg = "white",
-    bg = "red",
-    bold = true,
-    nocombine = true,
-})
-
-vim.api.nvim_set_hl(0, "LeapLabel", {
-    fg = "yellow",
-    bg = "None",
-    bold = true,
-    nocombine = true,
-})
-
 local hop = require("hop")
 local directions = require("hop.hint").HintDirection
 
@@ -59,115 +43,30 @@ vim.keymap.set("", "x", function()
     hop.hint_words()
 end, { remap = true, silent = true })
 
--- vim.keymap.set("", "f", function()
---     hop.hint_char1({
---         direction = directions.AFTER_CURSOR,
---         current_line_only = false,
---     })
--- end, { remap = true, silent = true })
--- vim.keymap.set("", "F", function()
---     hop.hint_char1({
---         direction = directions.BEFORE_CURSOR,
---         current_line_only = false,
---     })
--- end, { remap = true, silent = true })
--- vim.keymap.set("", "t", function()
---     hop.hint_char1({
---         direction = directions.AFTER_CURSOR,
---         current_line_only = false,
---         hint_offset = -1,
---     })
--- end, { remap = true, silent = true })
--- vim.keymap.set("", "T", function()
---     hop.hint_char1({
---         direction = directions.BEFORE_CURSOR,
---         current_line_only = false,
---         hint_offset = 1,
---     })
--- end, { remap = true, silent = true })
-
-function CreateNote()
-    local counter_file = vim.fn.expand("~/forest/counter.txt")
-    local counter = 0
-
-    local f = io.open(counter_file, "r")
-    if f then
-        counter = tonumber(f:read("*all")) or 0
-        f:close()
-    end
-
-    counter = counter + 1
-    local loc_counter = counter
-    local base36_counter = ""
-
-    while loc_counter > 0 do
-        local remainder = loc_counter % 36
-        base36_counter = string.char(
-            (remainder < 10 and 48 + remainder) or (87 + remainder)
-        ) .. base36_counter
-        loc_counter = math.floor(counter / 36)
-    end
-
-    while #base36_counter < 4 do
-        base36_counter = "0" .. base36_counter
-    end
-
-    local filename = "hzw-" .. base36_counter .. ".md"
-
-    print(counter)
-    f = io.open(counter_file, "w")
-    f:write(counter)
-    f:close()
-
-    vim.cmd("edit " .. filename)
-end
-
-function CreateJournalNote()
-    local counter_file = vim.fn.expand("~/notes/counter.txt")
-    local counter = 0
-
-    local f = io.open(counter_file, "r")
-    if f then
-        counter = tonumber(f:read("*all")) or 0
-        f:close()
-    end
-
-    counter = counter + 1
-    local loc_counter = counter
-    local base36_counter = ""
-
-    while loc_counter > 0 do
-        local remainder = loc_counter % 36
-        base36_counter = string.char(
-            (remainder < 10 and 48 + remainder) or (87 + remainder)
-        ) .. base36_counter
-        loc_counter = math.floor(counter / 36)
-    end
-
-    while #base36_counter < 4 do
-        base36_counter = "0" .. base36_counter
-    end
-
-    local filename = "jnr-" .. base36_counter .. ".md"
-
-    print(counter)
-    f = io.open(counter_file, "w")
-    f:write(counter)
-    f:close()
-
-    vim.cmd("edit " .. filename)
-end
-
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>fZ",
-    ":lua CreateNote()<CR>",
+    "<leader>gC",
+    "<cmd>Neogit commit<CR>",
     { noremap = true, silent = true }
 )
 
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>fz",
-    ":lua CreateJournalNote()<CR>",
+    "<leader>gk",
+    "<cmd>Neogit push<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>gK",
+    "<cmd>Neogit pull<CR>",
+    { noremap = true, silent = true }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>k",
+    '<cmd>lua require("kubectl").toggle()<cr>',
     { noremap = true, silent = true }
 )
