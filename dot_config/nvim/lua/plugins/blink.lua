cf67e84f3d7bd78c40bdad13fec6ea0e1b281bab
hamza wahed <hzd454@gmail.com>
2025-03-25 22:39:24 -0300

    update config

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,8 @@ return {
     dependencies = { "L3MON4D3/LuaSnip", version = "v2.3" },
     opts = function(_, opts)
         opts.completion.ghost_text.enabled = false
-        opts.completion.menu.auto_show = false
+        opts.completion.accept.auto_brackets.enabled = false
+        -- opts.completion.menu.auto_show = false
         opts.snippets = { preset = "luasnip" }
         opts.keymap = {
             preset = "default",
